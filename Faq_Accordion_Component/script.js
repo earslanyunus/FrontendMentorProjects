d00e0e20d6a37809bb0e804aa75fe21977ad2256
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2022-02-23 20:34:34 +0300

    some update

--- Faq_Accordion_Component/script.js
+++ Faq_Accordion_Component/script.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const question_box = document.querySelector('.question-box')
+
+question_box.addEventListener('click',evt => {
+
+    if (evt.target.classList.contains('fa-chevron-down') || evt.target.classList.contains('question-head-text')){
+      evt.target.closest('.question').classList.toggle('active')
+    }
+})
