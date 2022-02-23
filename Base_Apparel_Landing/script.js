3e9eb5c2ae0ec533373b729164851f7fd18acb4b
Yunus Emre Arslan <yyunus.earslan@gmail.com>
2022-02-23 19:47:51 +0300

    Finished apparel project

--- Base_Apparel_Landing/script.js
+++ Base_Apparel_Landing/script.js
@@ -1,1 +1,27 @@
 'use strict'
+
+
+const btn = document.querySelector('.btn');
+const form = document.querySelector('.form')
+const input = document.querySelector('.mailinput')
+const error = document.querySelector('.error p')
+const erricon = document.querySelector('.error-icon')
+
+
+ form.addEventListener('submit',evt => {
+     evt.preventDefault()
+     if (!input.checkValidity() || input.validity.valueMissing) {
+         error.textContent = 'Please provide a valid email'
+         error.classList.add('show')
+         erricon.classList.add('show')
+     }else if(!input.validity.valueMissing){
+         error.classList.remove('show')
+         error.textContent = ''
+         erricon.classList.remove('show')
+     }
+     console.log(input.validity)
+
+
+
+
+ })
